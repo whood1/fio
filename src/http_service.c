97c3af60d9b1f781fcd4d0bd231bc0abf1b78a9d
william <billylhood@gmail.com>
2022-08-12 17:52:32 -0400

    Reads HTML from file pointer to buffer, copies to a new string, and sends in HTML request body. Also attached javascript with script tag

--- src/http_service.c
+++ src/http_service.c
@@ -2,29 +2,59 @@
 #include "main.h"
 #include <stdio.h>
 
+void test() {
+  printf("test worked\n");
+  return;
+}
+
 /* TODO: edit this function to handle HTTP data and answer Websocket requests.*/
 static void on_http_request(http_s *h) {
   /* set a response and send it (finnish vs. destroy). */
 
   //open file
   FILE *fp;
+  char buffer[512];
+  int chars = 0;
+  //char current_char;
+  
   fp = fopen("../assets/index.html", "r");
   if (!fp) { printf("Error\n"); }
-  else { printf("Success\n"); }
+  else {
+
+    printf("Success\n");
+
+    for (int i = 0; i < 512; ++i) {
+      ++chars;
+      buffer[i] = getc(fp);
+      if (buffer[i] == EOF) {break;}
+      //printf("%c", buffer[i]);
+    }
+    
+  }
 
   
+  //  printf("%d", strlen);
+  char* html = (char *)malloc(strlen(buffer) + 1);
+  //char html[512];
+  strcpy(html, buffer);
+
+  //  printf("%s\n", buffer);
+ 
   
-  char html[] = "Content-Type: text/html\r\n\r\n<!DOCTYPE html><html><body><h1>World Hello</h1></body></html>";
   int size = sizeof(html);
-  size = size - 1;
+  size = chars - 1;
+  printf("\n\n>>HTML size %d<<\n\n%s\n", chars, html);
 
+  
+  //sets content type HTML
+  http_set_header(h, HTTP_HEADER_CONTENT_TYPE, http_mimetype_find("html", 4));
 
-  char body[] = "Testing";
-  int body_size = sizeof(body);
-  body_size = body_size - 1;
-  printf("body size %d\n", body_size);
+  //sets header with Data:data info, just testing
   http_set_header(h, fiobj_str_new("Test", 4), fiobj_str_new("Test", 4));
-  http_send_body(h, html, size);
+  
+  http_send_body(h, buffer, size);
+
+  free(html);
 }
 
 /* starts a listeninng socket for HTTP connections. */
