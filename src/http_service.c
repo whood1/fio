e33ae8f0fae71ab8859fc1ed5f648105f4de0eae
william <billylhood@gmail.com>
2022-08-15 14:46:59 -0400

    setting up websockets
    can connect from browser console, not from a link yet

--- src/http_service.c
+++ src/http_service.c
@@ -2,10 +2,12 @@
 #include "main.h"
 #include <stdio.h>
 
-void test() {
-  printf("test worked\n");
+static void on_http_upgrade(http_s *h) {
   
-  return;
+  printf("\n -WEBSOCKET UPGRADE--\n");
+  http_upgrade2ws(h);
+  printf("\n Success \n");
+  
 }
 
 /* TODO: edit this function to handle HTTP data and answer Websocket requests.*/
@@ -22,13 +24,18 @@ static void on_http_request(http_s *h) {
   FILE *fp;
   char buffer[512];
   int chars = 0;
-  //char current_char;
+  int open_websocket = 0;
 
   printf("\n strcmp: %d \n", strcmp(path_buf.data, "/chat.html"));
 
   if (strcmp(path_buf.data, "/chat.html") == 0) {
     printf("\n Chat Loaded\n");
     fp = fopen("../assets/chat.html", "r");
+
+    open_websocket = 1;
+    
+    //on_http_upgrade(h);
+    
   }
   else {
     printf("\nIndex loaded\n");
@@ -67,8 +74,15 @@ static void on_http_request(http_s *h) {
   //sets content type HTML
   http_set_header(h, HTTP_HEADER_CONTENT_TYPE, http_mimetype_find("html", 4));
 
-  //sets header with Data:data info, just testing
-  http_set_header(h, fiobj_str_new("Test", 4), fiobj_str_new("Test", 4));
+  //if websocket
+  if (open_websocket == 1) {
+
+    http_set_header(h, fiobj_str_new("connection", 10), fiobj_str_new("Upgrade", 7));
+    http_set_header(h, fiobj_str_new("upgrade", 7), fiobj_str_new("websocket", 9));
+
+    on_http_upgrade(h);
+    
+  }
   
   http_send_body(h, buffer, size);
 
@@ -80,6 +94,7 @@ void initialize_http_service(void) {
   /* listen for inncoming connections */
   if (http_listen(fio_cli_get("-p"), fio_cli_get("-b"),
                   .on_request = on_http_request,
+		  .on_upgrade = on_http_upgrade,
                   .max_body_size = fio_cli_get_i("-maxbd") * 1024 * 1024,
                   .ws_max_msg_size = fio_cli_get_i("-max-msg") * 1024,
                   .public_folder = fio_cli_get("-public"),
